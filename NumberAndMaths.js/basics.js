eab1216a9d1b1157a81ec3d13a9bc4722764dbf6
Lakde Maheshwari <maheshwarilakde@gmail.com>
2024-01-28 21:50:11 +0530

    "JavaScript Arrays"

--- NumberAndMaths.js/basics.js
+++ NumberAndMaths.js/basics.js
@@ -1,3 +1,15 @@
 const score = 400;
+console.log(score); //400
 const balance = new Number(100)
-console.log(balance);
+console.log(balance); //[Number : 100]
+console.log(balance.toString()) // 100
+console.log(balance.toFixed(2)); //100.00
+const hund = 1000000
+console.log(hund.toLocaleString('en-IN')); // in Indian System
+//  ******** Maths ********
+console.log(Math);
+console.log(Math.abs(-4)); //4
+console.log(Math.round(4.6)); //5
+console.log(Math.ceil(4.9)); // 5
+// Math.floor 
+// Math.max(),Math.min() and also Math.random()
